c315a0fa0c2fc72ca09303c0023760eda49c7d73
Demo <seimonmaiquez@gmail.com>
2025-09-03 14:25:14 +0800

    feat: add delete record and modify button interactions

--- static/js/admin.js
+++ static/js/admin.js
@@ -9,23 +9,30 @@ const imageInput = document.getElementById('image-input')
 const imageDisplay = document.getElementById('image-display')
 const studentDisplayButton = document.getElementById('student-record-button')
 const teacherDisplayButton = document.getElementById('teacher-record-button')
+const filterOptions = document.getElementById('filter')
+let currentTab = "student"
 
 const defaultProfilePicture = "../static/images/default_profile_picture.png"
 
+studentDisplayButton.disabled = true;
+
 studentDisplayButton.addEventListener('click', () => {
-    getStudentRecord()
+    currentTab = "student"
+    showRecords('/students')
+    filterOptions.value = "default"
     studentDisplayButton.disabled = true
     teacherDisplayButton.disabled = false
 })
 
 teacherDisplayButton.addEventListener('click', () => {
-    getTeacherRecord()
+    currentTab = "teacher"
+    showRecords('/teachers')
+    filterOptions.value = "default"
     studentDisplayButton.disabled = false
     teacherDisplayButton.disabled = true
 })
 
 imageInput.addEventListener('change', (event) => {
-    console.log("hehe")
     const file = event.target.files[0];
     if (file) {
         const reader = new FileReader();
@@ -40,7 +47,7 @@ imageInput.addEventListener('change', (event) => {
     
 });
 
-document.querySelector("form").addEventListener("submit", async (e) => {
+document.getElementById("main-form").addEventListener("submit", async (e) => {
     e.preventDefault()
 
     const formBody = e.target;
@@ -56,8 +63,22 @@ document.querySelector("form").addEventListener("submit", async (e) => {
 
     if (result.status){
         //notification here
-        prompt(result.message)
+        alert(result.message)
         formBody.reset()
+
+        formBody.action = '/register'
+
+        const submitButton = document.getElementById('submit-user-button');
+        const cancelButton = document.getElementById('cancel-user-button');
+        cancelButton.hidden = true;
+        submitButton.value = "Submit"
+        
+        if (currentTab == "student"){
+            showRecords('/students')
+        }
+        else if (currentTab == "teacher"){
+            showRecords('/teachers')
+        }
 
     }
     else{
@@ -67,6 +88,27 @@ document.querySelector("form").addEventListener("submit", async (e) => {
             })
             return
         }
+        alert("Error: " + result.message)
+    }
+})
+
+document.getElementById("filter").addEventListener("change", async (e) => {
+    const filterValue = e.target.value;
+    const role = currentTab
+
+    const url = `/filter_record/${role}/${filterValue}`
+
+    const response = await fetch(url)
+
+    const result = await response.json()
+
+    if (result.status){
+        tableBody.innerHTML = ""
+        result.data.forEach(data => {
+            addRow(data.id, data.fname, data.lname, data.email, data.image, data.role)
+        })
+    }
+    else{
         alert("Error: " + result.message)
     }
 })
@@ -88,38 +130,22 @@ closeProfileButton.addEventListener("click", () => {
     logOutButton.style.alignSelf = "center";            
 });
 
-async function getStudentRecord(){
-    const response = await fetch('/students')
+async function showRecords(apiRoute){
+    const response = await fetch(apiRoute)
     const result = await response.json()
 
     tableBody.innerHTML = ""
 
     if (result.status){
-        result.data.forEach(student => {
-            addRow(student.id, student.fname, student.lname, student.email, student.image, student.role)
+        result.data.forEach(data => {
+            addRow(data.id, data.fname, data.lname, data.email, data.image, data.role)
         })
+
     }
     else{
         alert("Error: " + result.message)
     }
 
-}
-
-async function getTeacherRecord(){
-    const response = await fetch('/teachers')
-    const result = await response.json()
-
-    tableBody.innerHTML = ""
-
-    if (result.status){
-        result.data.forEach(teacher => {
-            addRow(teacher.id, teacher.fname, teacher.lname, teacher.email, teacher.image, teacher.role)
-
-        })
-    }
-    else{
-        alert("Error: " + result.message)
-    }
 }
 
 function addRow(user_id, user_fname, user_lname, user_email, user_image, role){
@@ -181,7 +207,10 @@ function addRow(user_id, user_fname, user_lname, user_email, user_image, role){
         populateForm(user_image, user_id, user_fname, user_lname, user_email, role.toLowerCase())
     })
 
- 
+    deleteBtn.addEventListener('click', () => {
+        deleteUser(user_id, role.toLowerCase())
+    })
+
 
     // Append all cells to the row
     newRow.appendChild(imgTd);
@@ -194,7 +223,7 @@ function addRow(user_id, user_fname, user_lname, user_email, user_image, role){
 }
 
 function populateForm(image, id, fname, lname, email, role){
-    const mainForm = document.querySelector('form')
+    const mainForm = document.getElementById('main-form')
     const submitActionContainer = document.getElementById('submit-actions')
     const imageInput = document.getElementById('image-display');
     const idInput = document.getElementById('id');
@@ -204,6 +233,10 @@ function populateForm(image, id, fname, lname, email, role){
     const roleInput = document.getElementById('role');
 
     const submitButton = document.getElementById('submit-user-button')
+    const cancelButton = document.getElementById('cancel-user-button')
+    cancelButton.hidden = false
+
+    submitButton.disabled = true;
 
     const originalId = document.createElement("input");
     originalId.type = "text"
@@ -215,22 +248,42 @@ function populateForm(image, id, fname, lname, email, role){
 
     mainForm.action = "modify_user";
 
-    imageInput.src = image;
+    if (image){
+        imageInput.src = image;
+    }
+    else{
+        imageInput.src = defaultProfilePicture;
+    }
     idInput.value = id;
     fnameInput.value = fname;
     lnameInput.value = lname;
     emailInput.value = email;
     roleInput.value = role;
 
-    submitButton.value = "Save";
+    function updateSubmitButtonState() {
+        const isIdSame = idInput.value == id;
+        const isFnameSame = fnameInput.value == fname;
+        const isLnameSame = lnameInput.value == lname;
+        const isEmailSame = emailInput.value == email;
+        const allFieldsAreTheSame = isIdSame && isFnameSame && isLnameSame && isEmailSame;
+        
+        submitButton.disabled = allFieldsAreTheSame;
+    }
 
-    const cancelButton = document.createElement("button")
-    cancelButton.textContent = "Cancel"
-    cancelButton.setAttribute('id', "cancel-user-button")
+    idInput.addEventListener('input', updateSubmitButtonState);
+    fnameInput.addEventListener('input', updateSubmitButtonState);
+    lnameInput.addEventListener('input', updateSubmitButtonState);
+    emailInput.addEventListener('input', updateSubmitButtonState);
+
+    submitButton.value = "Save";
 
     submitActionContainer.appendChild(cancelButton)
 
     cancelButton.addEventListener('click', () => {
+        mainForm.removeChild(originalId)
+        document.querySelectorAll(".edit-buttons").forEach(element => {
+            element.removeEventListener('click', populateForm)
+        })
         cancelModify()
     })   
 
@@ -241,11 +294,12 @@ function populateForm(image, id, fname, lname, email, role){
 }
 
 function cancelModify(){
-    const submitActionContainer = document.getElementById('submit-actions');
     const cancelButton = document.getElementById("cancel-user-button");
     const submitButton = document.getElementById("submit-user-button");
 
-    submitActionContainer.removeChild(cancelButton);
+    submitButton.value = "Submit";
+    submitButton.disabled = false;
+    cancelButton.hidden = true;
     document.querySelector("form").reset();
 
     document.querySelectorAll(".edit-buttons, .delete-buttons").forEach(element => {
@@ -255,7 +309,82 @@ function cancelModify(){
     imageDisplay.src = defaultProfilePicture; 
     submitButton.textContent = "Submit";
 
-
 }
 
-getStudentRecord()
+function deleteUser(id, role){
+    const formContainer = document.createElement('form');
+    const inputContainer = document.createElement('div');
+    const submitButton = document.createElement('input');
+    const cancelButton = document.createElement('input');
+    const buttonContainer = document.createElement('aside');
+    const statement = document.createElement('p');
+    const idInput = document.createElement('input');
+    const roleInput = document.createElement('input');
+
+    idInput.type = 'text';
+    idInput.value = id;
+    idInput.hidden = true;
+    idInput.name = 'id';
+    roleInput.type = 'text';
+    roleInput.value = role;
+    roleInput.hidden = true;
+    roleInput.name = 'role';
+
+    formContainer.action = '/delete_user';
+    formContainer.method = 'POST';
+    formContainer.setAttribute('id', 'delete-form');
+
+    inputContainer.setAttribute('id', 'delete-input-container');
+
+    submitButton.type = 'submit';
+    submitButton.value = 'Yes';
+    cancelButton.type = 'button';
+    cancelButton.value = "No";
+    statement.textContent = `Are you sure you want to delete user ${id}`;
+
+
+    cancelButton.addEventListener('click', () => {
+        if (formContainer.parentNode) {
+            formContainer.parentNode.removeChild(formContainer);
+        }
+    })
+
+    buttonContainer.style.display = "flex";
+    buttonContainer.append(submitButton, cancelButton);
+    inputContainer.append(statement, buttonContainer);
+    formContainer.append(inputContainer, idInput, roleInput);
+    document.body.appendChild(formContainer)
+
+    formContainer.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        const formBody = e.target;
+        const formData = new FormData(formBody);
+
+        const response = await fetch('/delete_user', {
+            method: "POST",
+            body: formData
+        });
+
+        const result = await response.json()
+
+        if (result.status){
+            formBody.remove();
+            alert(result.message)
+
+            if (currentTab == "student"){
+                showRecords('/students')
+            }
+            else if (currentTab == "teacher"){
+                showRecords('/teachers')
+            }
+
+        }
+        else{
+            alert("Error: " + result.message)
+        }
+    })
+}
+
+
+showRecords('/students')
