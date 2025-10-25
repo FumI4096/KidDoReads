7faae17fd7e989e0f2921852ea4705652bc6a02e
Demo <seimonmaiquez@gmail.com>
2025-10-25 20:31:11 +0800

    test: add attempt and score retrievals from students

--- api/attempt_api.py
+++ api/attempt_api.py
@@ -4,7 +4,7 @@ from modules.utils import get_db
 attempt_bp = Blueprint('attempt_bp', __name__)
 
 @attempt_bp.route('/attempt', methods=['POST'])
-def create_student_attempt():
+def game_attempt():
     try:
         db = get_db()
         content_id = request.form.get('content_id')
@@ -17,6 +17,59 @@ def create_student_attempt():
             return jsonify({"status": status})
         else:
             return jsonify({"status": status, "message": message})
+        
+    except Exception as e:
+        return jsonify({"status": False, "message": str(e)})
+    
+@attempt_bp.route('/attempts/<int:teacher_id>/<int:content_type>', methods=['GET'])
+def student_progress_by_contents(teacher_id, content_type):
+    try:
+        db = get_db()
+        
+        status, results = db.get_student_progress_by_contents(teacher_id, content_type)
+        
+        rows = results
+        attempts = []
+        for row in rows:
+            attempts.append({
+                "content_id": row[0],
+                "content_title": row[1],
+                "completed_students": row[2],
+                "total_students": row[3],
+                "progress": row[4]
+            })
+        
+        if status:
+            return jsonify({"status": status, "attempts": attempts})
+        else:
+            return jsonify({"status": status, "message": "Failed to retrieve attempts"})
+        
+    except Exception as e:
+        return jsonify({"status": False, "message": str(e)})
+    
+@attempt_bp.route('/attempts/<int:content_id>', methods=['GET'])
+def student_scores_by_contents(content_id):
+    try:
+        db = get_db()
+        
+        status, results = db.get_student_scores_by_content_id(content_id)
+        
+        rows = results
+        scores = []
+        for row in rows:
+            scores.append({
+                "student_id": row[0],
+                "student_name": row[1],
+                "student_attempts": row[2],
+                "student_highest_score": row[3],
+                "student_lowest_score": row[4],
+                "total_questions": row[5]
+            })
+        
+        if status:
+            return jsonify({"status": status, "scores": scores})
+        else:
+            return jsonify({"status": status, "message": "Failed to retrieve attempts"})
         
     except Exception as e:
         return jsonify({"status": False, "message": str(e)})
