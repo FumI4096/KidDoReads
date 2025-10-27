4ce1d6866ef6184dfaa8cf7a1049b586232b240f
Demo <seimonmaiquez@gmail.com>
2025-10-27 18:58:12 +0800

    fix: add parameters

--- routes/status_error_routes.py
+++ routes/status_error_routes.py
@@ -4,21 +4,21 @@ from flask import Blueprint, render_template
 errors = Blueprint('errors', __name__)
 
 @errors.app_errorhandler(400)
-def bad_request():
+def bad_request(e):
     return render_template('error.html', type="400", error="Bad Request"), 400
 
 @errors.app_errorhandler(401)
-def unauthorized():
+def unauthorized(e):
     return render_template('error.html', type="401", error="Unauthorized Access"), 401
 
 @errors.app_errorhandler(403)
-def forbidden():
+def forbidden(e):
     return render_template('error.html', type="403", error="Inaccessible to Enter this Page"), 403
 
 @errors.app_errorhandler(404)
-def not_found():
+def not_found(e):
     return render_template('error.html', type="404", error="Page not Found"), 404
 
 @errors.app_errorhandler(405)
-def method_not_allowed():
+def method_not_allowed(e):
     return render_template('error.html', type="405", error="Page not Available"), 405
