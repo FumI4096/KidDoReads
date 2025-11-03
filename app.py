90c9d209271fed6ec6670c6c33e94c010de453f2
Demo <seimonmaiquez@gmail.com>
2025-11-04 00:33:24 +0800

    feat: register chatbot API blueprint in the application

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from api.user_api import user_bp
 from api.contents_api import contents_bp
 from api.attempt_api import attempt_bp
 from api.tts_api import tts_bp
+from api.chatbot_api import chatbot_bp
 from routes.status_error_routes import errors
 from routes.home_routes import home_bp
 from routes.edit_games_routes import edit_games_bp
@@ -39,6 +40,7 @@ app.register_blueprint(user_bp)
 app.register_blueprint(contents_bp)
 app.register_blueprint(attempt_bp)
 app.register_blueprint(tts_bp)
+app.register_blueprint(chatbot_bp)
 
 
 if __name__ == '__main__':
