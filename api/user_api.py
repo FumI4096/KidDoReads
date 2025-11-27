747efba433aa5890075b1dbd5dfdd38fc5216a3c
Mimon <sesmaiquez@gmail.com>
2025-11-27 21:22:36 +0800

    fix: change response in create_admin function from print to return

--- api/user_api.py
+++ api/user_api.py
@@ -244,6 +244,6 @@ def create_admin():
         ''', ('Admin', 'User', 'admin@kiddoreads.com', hashed_password, 3, None))
         
         db.connection.commit()
-        print ("Admin user created!")
+        return "Admin user created!"
     except Exception as e:
-        print (f"Error: {e}")
+        return f"Error: {e}"
