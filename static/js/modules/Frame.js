3ec0ba10aa6448ce05fd1f59511dc876261cda33
Mimon <sesmaiquez@gmail.com>
2025-11-29 17:13:31 +0800

    feat: implement idle animation with preloaded frames for smoother performance

--- static/js/modules/Frame.js
+++ static/js/modules/Frame.js
@@ -1,14 +1,44 @@
 
-const totalFrames = 48; // Replace with the number of frames you have
-let frame = 0;
-const img = document.getElementById('owl');
+let owlData;
+let framesPreloaded = [];
 
-function pad(num, size) {
-    let s = "00000" + num;
-    return s.substr(s.length - size);
-}
+fetch('../../static/animation/idle-owl.json')
+    .then(response => response.json())
+    .then(data => {
+        owlData = data;
+        preloadFrames();
+    });
 
-setInterval(() => {
-    img.src = `static/animation/frame/frame_${pad(frame, 5)}.png`;
-    frame = (frame + 1) % totalFrames; // loops continuously
-}, 1000 / 24); // 24 fps, adjust as needed
+    // Preload all frames to avoid flickering
+    function preloadFrames() {
+        owlData.idle.forEach(path => {
+            const img = new Image();
+            img.src = path;
+            framesPreloaded.push(img);
+        });
+        startIdleAnimation();
+    }
+
+    function startIdleAnimation() {
+        const img = document.getElementById('owl');
+        let frame = 0;
+        const totalFrames = owlData.idle.length;
+        const fps = 24;
+        const interval = 1000 / fps;
+        let lastTime = 0;
+
+        function animate(time) {
+            if (!lastTime) lastTime = time;
+            const elapsed = time - lastTime;
+
+            if (elapsed > interval) {
+            img.src = framesPreloaded[frame].src; // use preloaded image
+            frame = (frame + 1) % totalFrames;    // seamless loop
+            lastTime = time;
+            }
+
+            requestAnimationFrame(animate);
+        }
+
+        requestAnimationFrame(animate);
+    }
